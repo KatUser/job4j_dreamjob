010f316c4ec6b351393214a6cb00f651ac7ce2e7
KatUser <ekaterina.shirokikh@gmail.com>
2024-06-18 18:12:42 +0700

    4. Thymeleaf, Циклы. [#504841]

---
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -0,0 +1,80 @@
+package ru.job4j.dreamjob.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.job4j.dreamjob.model.Candidate;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public class MemoryCandidateRepository implements CandidateRepository {
+
+    private static final MemoryCandidateRepository INSTANCE = new MemoryCandidateRepository();
+
+    private int nextId = 1;
+
+    private final Map<Integer, Candidate> candidates = new HashMap<>();
+
+    private MemoryCandidateRepository() {
+        save(new Candidate(0,
+                "Tom",
+                "smart, knows some SQL",
+                LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS)));
+        save(new Candidate(0,
+                "Jerry",
+                "not smart, likes cheese and Golang",
+                LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS)));
+        save(new Candidate(0,
+                "Woodie",
+                "nice, but too loud; uses C++",
+                LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS)));
+        save(new Candidate(0,
+                "Piggie",
+                "is a small piglet; enjoys Assembler",
+                LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS)));
+        save(new Candidate(0,
+                "Minnie",
+                "is a small mouse that codes in Java",
+                LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS)));
+    }
+
+    public static MemoryCandidateRepository getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public Candidate save(Candidate candidate) {
+        candidate.setId(nextId++);
+        candidates.put(candidate.getId(), candidate);
+        return candidate;
+    }
+
+    @Override
+    public void deleteById(int id) {
+        candidates.remove(id);
+    }
+
+    @Override
+    public boolean update(Candidate candidate) {
+        return candidates.computeIfPresent(candidate.getId(),
+                (id, oldCandidate) -> new Candidate(
+                        oldCandidate.getId(),
+                        candidate.getName(),
+                        candidate.getDescription(),
+                        candidate.getCreationDate())) != null;
+    }
+
+    @Override
+    public Optional<Candidate> findById(int id) {
+        return  Optional.ofNullable(candidates.get(id));
+    }
+
+    @Override
+    public Collection<Candidate> findAll() {
+        return candidates.values();
+    }
+}
---
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
@@ -0,0 +1,76 @@
+package ru.job4j.dreamjob.repository;
+
+import ru.job4j.dreamjob.model.Vacancy;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class MemoryVacancyRepository implements VacancyRepository {
+
+    private static final MemoryVacancyRepository INSTANCE = new MemoryVacancyRepository();
+
+    private int nextId = 1;
+
+    private final Map<Integer, Vacancy> vacancies = new HashMap<>();
+
+    private MemoryVacancyRepository() {
+        save(new Vacancy(0, "Intern Java Developer",
+                "We pay nothing, you get experience",
+                LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS)));
+        save(new Vacancy(0, "Junior Java Developer",
+                "You work for some food and experience",
+                LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS)));
+        save(new Vacancy(0, "Junior+ Java Developer",
+                "You work for food, cookies and experience",
+                LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS)));
+        save(new Vacancy(0, "Middle Java Developer",
+                "You work for salary, food and experience",
+                LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS)));
+        save(new Vacancy(0, "Middle+ Java Developer",
+                "Description is not available here",
+                LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS)));
+        save(new Vacancy(0, "Senior Java Developer",
+                "Don't even call us",
+                LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS)));
+    }
+
+    public static MemoryVacancyRepository getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public Vacancy save(Vacancy vacancy) {
+        vacancy.setId(nextId++);
+        vacancies.put(vacancy.getId(), vacancy);
+        return vacancy;
+    }
+
+    @Override
+    public void deleteById(int id) {
+        vacancies.remove(id);
+    }
+
+    @Override
+    public boolean update(Vacancy vacancy) {
+        return vacancies.computeIfPresent(vacancy.getId(),
+                (id, oldVacancy) -> new Vacancy(
+                        oldVacancy.getId(),
+                        vacancy.getTitle(),
+                        vacancy.getDescription(),
+                        vacancy.getCreationDate())) != null;
+    }
+
+    @Override
+    public Optional<Vacancy> findById(int id) {
+        return Optional.ofNullable(vacancies.get(id));
+    }
+
+    @Override
+    public Collection<Vacancy> findAll() {
+        return vacancies.values();
+    }
+}
